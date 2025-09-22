ae9ee2decdf4d0561f38bc7aa6f382ddb27f01a2
Zuha-liaqat <zuhaliaqat996@gmail.com>
2025-09-22 16:54:48 +0500

    Changes

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,20 +1,27 @@
 export default function Hero() {
   return (
     <>
-    <section className="flex flex-col items-center justify-center text-center text-white py-20">
-      <h2 className="text-4xl font-bold">Hi, I’m Zuha Liaqat👋</h2>
-      <p className="mt-4 text-lg ">
-        Frontend Developer | React | TailwindCSS
-      </p>
-      <a
-        href="#projects"
-        className="mt-6 px-6 py-3 bg-white text-black rounded-lg shadow hover:bg-[#32a4c3] hover:border border-white transition"
-      >
-        View My Work
-      </a>
-    
-    </section>
-    <hr className="mt-10  border-t-2 border-white" />
+      <section className="flex flex-col items-center justify-center text-center text-white py-20">
+        <h2 className="text-4xl font-bold">Hi, I’m Zuha Liaqat👋</h2>
+        <p className="mt-4 text-lg ">
+          Frontend Developer | React | TailwindCSS
+        </p>
+        <div className="mt-6 flex gap-4">
+          <a href="/CV.pdf" download="CV.pdf">
+            <button className="border px-4 py-2  text-white rounded-lg hover:bg-white hover:text-black transition">
+              Download CV
+            </button>
+          </a>
+
+          <a
+            href="#projects"
+            className="px-6 py-2 bg-white text-black rounded-lg shadow hover:bg-[#32a4c3] hover:border border-white transition"
+          >
+            View My Work
+          </a>
+        </div>
+      </section>
+      <hr className="mt-10  border-t-2 border-white" />
     </>
   );
 }
